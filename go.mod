ab4e0ea5b51b2a649284fc30ea64df002bd891be
Adam Dyer <adamdyer@gmail.com>
2024-08-26 09:48:10 -0400

    added pg container configuration

--- go.mod
+++ go.mod
@@ -71,8 +71,8 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	github.com/testcontainers/testcontainers-go v0.31.0 // indirect
-	github.com/testcontainers/testcontainers-go/modules/postgres v0.31.0 // indirect
+	github.com/testcontainers/testcontainers-go v0.31.0
+	github.com/testcontainers/testcontainers-go/modules/postgres v0.31.0
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
--- test-util.go
+++ test-util.go
@@ -11,16 +11,27 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func CreatePostgresqlContainer(t *testing.T) *PostgreSqlConfig {
+func CreateDefaultPostgresqlContainer(t *testing.T) *PostgreSqlConfig {
+
+	config := PostgreSqlConfig{
+		Database: "test-db",
+		User:     "postgres",
+		Password: "postgres",
+	}
+
+	return CreatePostgresqlContainer(t, &config)
+}
+
+func CreatePostgresqlContainer(t *testing.T, config *PostgreSqlConfig) *PostgreSqlConfig {
 
 	ctx := context.Background()
 
 	pgContainer, err := postgres.RunContainer(ctx,
 		testcontainers.WithImage("postgres:15.3-alpine"),
 		// postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
-		postgres.WithDatabase("test-db"),
-		postgres.WithUsername("postgres"),
-		postgres.WithPassword("postgres"),
+		postgres.WithDatabase(config.Database),
+		postgres.WithUsername(config.User),
+		postgres.WithPassword(config.Password),
 		testcontainers.WithWaitStrategy(
 			wait.ForLog("database system is ready to accept connections").
 				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
